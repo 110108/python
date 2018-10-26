e38d39821318b19efc7287957535d998ed8144f5
110108 <31996737+110108@users.noreply.github.com>
2018-10-26 07:38:51 -0400

    Update Random shapes.py

--- Random shapes.py
+++ Random shapes.py
@@ -24,12 +24,39 @@ def sqr(num, angle):
     right(angle)
 
 def pent(num, angle):
+    left(angle)
     for i in range(5):
-        
+        forward(num)
+        left(108)
+    right(angle)
 
-Pick up the pen.
-Jump to a random x, y position (x between -450 and 450 and y between -400 and 400).
-Put down the pen.
-Start a random color fill (must use 10 or more colors/shades).
-Draw a small shape (triangle, square, pentagon, or hexagon).
-End the color fill.
+def hex(num, angle):
+    left(angle)
+    for i in range(6):
+        forward(num)
+        left(120)
+    right(angle)
+    
+shapes = [tri, sqr, pent, hex]
+
+def changeShape():
+    num = random.randint(0, 3)
+    if(num==0):
+        tri(random.randint(0, 50)
+
+def move():    
+    turtle.penup
+    turtle.goto(random.randint(-450,450),random.randint(-400,400))
+    turtle.pendown
+
+while(True):
+    move()
+    changeShape()
+
+
+#Pick up the pen.
+#Jump to a random x, y position (x between -450 and 450 and y between -400 and 400).
+#Put down the pen.
+#Start a random color fill (must use 10 or more colors/shades).
+#Draw a small shape (triangle, square, pentagon, or hexagon).
+#End the color fill.
