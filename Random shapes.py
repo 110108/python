1804962af4319accacb72272bb1c45fa84fbe43f
110108 <31996737+110108@users.noreply.github.com>
2018-11-01 07:44:20 -0400

    Update Random shapes.py

--- Random shapes.py
+++ Random shapes.py
@@ -1,7 +1,12 @@
 import turtle
 import random
 
+turtle.speed(10)
+turtle.colormode(255)
+
 def tri(num, angle):
+    turtle.begin_fill()
+    turtle.fillcolor((random.randint(0,255)),((random.randint(0,255))),(random.randint(0,255)))
     turtle.left(angle)
     turtle.forward(num)
     turtle.left(120)
@@ -10,8 +15,11 @@ def tri(num, angle):
     turtle.forward(num)
     turtle.left(120)
     turtle.right(angle)
+    turtle.end_fill()
     
 def sqr(num, angle):
+    turtle.begin_fill()
+    turtle.fillcolor((random.randint(0,255)),((random.randint(0,255))),(random.randint(0,255)))
     turtle.left(angle)
     turtle.forward(num)
     turtle.left(90)
@@ -22,49 +30,50 @@ def sqr(num, angle):
     turtle.forward(num)
     turtle.left(90)
     turtle.right(angle)
+    turtle.end_fill()
 
 def pent(num, angle):
+    turtle.begin_fill()
+    turtle.fillcolor((random.randint(0,255)),((random.randint(0,255))),(random.randint(0,255)))
     turtle.left(angle)
     for i in range(5):
         turtle.forward(num)
-        turtle.left(108)
+        turtle.left(72)
     turtle.right(angle)
+    turtle.end_fill()
 
 def hexa(num, angle):
+    turtle.begin_fill()
+    turtle.fillcolor((random.randint(0,255)),((random.randint(0,255))),(random.randint(0,255)))
     turtle.left(angle)
     for i in range(6):
         turtle.forward(num)
-        turtle.left(120)
+        turtle.left(60)
     turtle.right(angle)
+    turtle.end_fill()
 
 def changeShape():
     num = random.randint(0, 3)
     
     if(num==0):
-        tri(random.randint(0, 50),random.randint(0,360))
+        tri(random.randint(10, 50),random.randint(0,360))
     
     if(num==1):
-        sqr(random.randint(0, 50),random.randint(0,360))
+        sqr(random.randint(10, 50),random.randint(0,360))
     
     if(num==2):
-        pent(random.randint(0, 50),random.randint(0,360))
+        pent(random.randint(10, 50),random.randint(0,360))
     
-    if(num==3):
-        hexa(random.randint(0, 50),random.randint(0,360))
+    if(num==2 or num==3):
+        hexa(random.randint(10, 50),random.randint(0,360))
 
 def move():    
     turtle.penup()
     turtle.goto(random.randint(-450,450),random.randint(-400,400))
     turtle.pendown()
 
-while(True):
+for i in range(200):
     move()
     changeShape()
 
-
-#Pick up the pen.
-#Jump to a random x, y position (x between -450 and 450 and y between -400 and 400).
-#Put down the pen.
 #Start a random color fill (must use 10 or more colors/shades).
-#Draw a small shape (triangle, square, pentagon, or hexagon).
-#End the color fill.
