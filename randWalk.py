189a72c47fa3606c6222d32dacfb66c2ae96dde1
110108 <31996737+110108@users.noreply.github.com>
2018-11-14 10:28:54 -0500

    Update randWalk.py

--- randWalk.py
+++ randWalk.py
@@ -5,13 +5,29 @@ turtle.colormode(255)
 turtle.speed(0)
 turtle.tracer(0)
 turtle.hideturtle()
-turning = True
+turning = False
+
+def drawBord():
+    turtle.penup()
+    turtle.goto(450,400)
+    turtle.setheading(270)
+    turtle.pendown()
+    turtle.forward(800)
+    turtle.right(90)
+    turtle.forward(900)
+    turtle.right(90)
+    turtle.forward(800)
+    turtle.right(90)
+    turtle.forward(900)
+    turtle.penup()
+    turtle.home()
+    turtle.pendown()
 
 def bounce():
     if((x>=450 or x<=-450)or(y>=400 or y<=-400)):
         turning = False
-    #if((x<=400 and x>=-400)and(y<=350 and y>=-350)):
-        #turning = True
+    if((x<400 and x>-400)and(y<350 and y>-350)):
+        turning = True
 
 def turn():
     if(turning==True):
@@ -22,6 +38,8 @@ def turn():
 def randMove():
     turtle.forward(random.randint(5,50))
 
+drawBord()
+turtle.goto(300,300)
 while True:
     x=turtle.xcor()
     y=turtle.ycor()
