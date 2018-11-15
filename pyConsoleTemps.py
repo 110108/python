a98720bac049d18ec4b81c936784298c909ac244
110108 <31996737+110108@users.noreply.github.com>
2018-11-15 08:27:53 -0500

    Update pyConsoleTemps.py

---
+++ pyConsoleTemps.py
@@ -0,0 +1,19 @@
+# Repeat forever
+# Ask "Enter 1 to convert Fahrenheit to Celsius, 2 to convert Celsius to Fahrenheit, or Q to Quit: "
+# if choice = 1
+# "What Fahrenheit temperature would you like to convert? "
+# fahrenheitTemp - 32 * 5 / 9
+# "That temperature in Celsius is ____"
+# elif choice = 2
+# "What Celsius temperature would you like to convert? "
+# celsiusTemp * 9 / 5 + 32
+# "That temperature in Fahrenheit is ____"
+# else
+# "PLEASE ENTER EITHER 1, 2, OR Q"
+import turtle
+import random
+
+turtle.colormode(255)
+turtle.speed(0)
+turtle.tracer(0)
+turtle.hideturtle()
