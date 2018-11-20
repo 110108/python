bc97376d1302bc00c2ec396761cd3de7b02d55a1
110108 <31996737+110108@users.noreply.github.com>
2018-11-20 08:02:27 -0500

    Update Pseudocode3PPong.py

---
+++ Pseudocode3PPong.py
@@ -0,0 +1,8 @@
+# define a circular playing field
+# divide field into radial thirds
+# players move on edge of circle
+# movement fields overlap by 10px 
+# ball starts at center and moves in a random direction until bounces off a player
+# if player misses and ball leaves field the other two players score a half point
+# if ball was in range of both players and niether gets it the third player wins a full point
+# ball slowly speeds up
