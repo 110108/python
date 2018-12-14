e6e2cff31e3aea00c178af8d87cbcb877271c5d2
110108 <31996737+110108@users.noreply.github.com>
2018-12-14 15:01:28 -0500

    Update pythonTurtleScene.py

--- pythonTurtleScene.py
+++ pythonTurtleScene.py
@@ -6,8 +6,26 @@ turtle.speed(0)
 turtle.tracer(0)
 turtle.hideturtle()
 
-def seaweedLeaf(width, height):
-    direction = 
-    for h in range height:
-        for i in range 180:
-            turtle.forward
+def seaweedLeaf(x,width, height):
+    turtle.goto(x,-500)
+    turtle.pendown
+    turtle.begin_fill()
+    turtle.color(random.randint(0,13),random.randint(50,230),random.randint(0,26))
+    direction = 1
+    turtle.left(25)
+    for i in range (0, height):
+        for i in range (0, 65):
+            turtle.forward(width/5)
+            turtle.left(direction*2)
+        direction*=-1
+    turtle.seth(270)
+    turtle.forward((height*104)/width)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.update()
+
+def bubble(x,y,size):
+    turtle.goto(x,y)
+    turtle.pendown()
+    #TODO: finish this function
+#TODO: write write function
